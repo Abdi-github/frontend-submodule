64cc1ea258387829b412b140c10d5e8db68f96b2
abdigit <abdulkadirahmed1985@gmail.com>
2020-11-11 02:03:50 +0100

    redux constants actions and reducers setup homepage

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,15 +1,53 @@
-import React from "react";
-import { data } from "../../data";
+// import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+// import { data } from "../../data";
 import Product from "../Product";
+// import axios from "axios";
+import Message from "../Message";
+import Loading from "../Loading";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../../actions/productActions";
 
 function HomePage() {
+  // const [products, setProducts] = useState([]);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(false);
+  /*
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  */
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, products, error } = productList;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
   return (
     <div>
-      <div className="row center">
-        {data.products.map((product) => (
-          <Product key={product._id} product={product} />
-        ))}
-      </div>
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/pages/ProductPage.js
+++ src/components/pages/ProductPage.js
@@ -51,7 +51,7 @@ function ProductPage(props) {
                     {countInStock > 0 ? (
                       <span className="success">In stock </span>
                     ) : (
-                      <span className="error">Unavailable</span>
+                      <span className="danger">Unavailable</span>
                     )}
                   </div>
                 </div>
